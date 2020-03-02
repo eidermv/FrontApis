1412b99ffe55c1c08358f91612999d082b563ae0
Eider Arley Muñoz <eider205v@gmail.com>
2020-03-02 02:12:54 -0500

    EAMV Creacion de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './componentes/login/login/login.component';
+import {AgregarComponent} from './componentes/producto/agregar/agregar.component';
+import {EditarComponent} from './componentes/producto/editar/editar.component';
+import {ListarComponent} from './componentes/producto/listar/listar.component';
+import {VerComponent} from './componentes/producto/ver/ver.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'producto/agregar', component: AgregarComponent},
+  {path: 'producto/editar', component: EditarComponent},
+  {path: 'producto/listar', component: ListarComponent},
+  {path: 'producto/ver', component: VerComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './componentes/app/app.component';
+import { AgregarComponent } from './componentes/producto/agregar/agregar.component';
+import { EditarComponent } from './componentes/producto/editar/editar.component';
+import { ListarComponent } from './componentes/producto/listar/listar.component';
+import { VerComponent } from './componentes/producto/ver/ver.component';
+import { LoginComponent } from './componentes/login/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarComponent,
+    EditarComponent,
+    ListarComponent,
+    VerComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
